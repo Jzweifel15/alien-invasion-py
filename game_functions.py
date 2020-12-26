07a0e17dc5a1d9122ba85c4bd51dcdfbe7b63b02
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-26 00:53:14 -0500

    Reorganization
    reorganized some code by moving some of the game's functionality into its own function

--- game_functions.py
+++ game_functions.py
@@ -6,20 +6,10 @@ def check_events(ship):
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit() 
-
-    elif event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_RIGHT:
-        # Move the ship to the right
-        ship.moving_right = True 
-      elif event.key == pygame.K_LEFT:
-        # Move the ship to the left
-        ship.moving_left = True 
-
+    elif event.type == pygame.KEYDOWN: 
+      check_keydown_events(event, ship)
     elif event.type == pygame.KEYUP:
-      if event.key == pygame.K_RIGHT:
-        ship.moving_right = False 
-      elif event.key == pygame.K_LEFT:
-        ship.moving_left = False 
+      check_keyup_events(event, ship) 
 
 
 def update_screen(ai_settings, screen, ship):
@@ -30,4 +20,22 @@ def update_screen(ai_settings, screen, ship):
 
   # Make the most recently drawn screen visible
   pygame.display.flip()
+
+
+def check_keydown_events(event, ship):
+  """ Respond to keypressed. """
+  if event.key == pygame.K_RIGHT:
+    # Move the ship to the right
+    ship.moving_right = True 
+  elif event.key == pygame.K_LEFT:
+    # Move the ship to the left
+    ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+  """ Respond to key releases. """
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = False 
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = False 
 
