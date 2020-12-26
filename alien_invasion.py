c4fe8075c67618b7281da78e90a7c255da129d73
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-26 18:49:47 -0500

    Draw Play Button
    play button is drawn to the screen on start up

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 from settings import Settings 
 from ship import Ship
 from game_stats import GameStats
+from button import Button
 
 import game_functions as game_fns
 
@@ -13,6 +14,9 @@ def run_game():
   ai_settings = Settings()
   screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
+
+  # Make the Play button
+  play_button = Button(ai_settings, screen, "Play")
 
   # Create an instance of store game statistics
   stats = GameStats(ai_settings)
@@ -34,7 +38,7 @@ def run_game():
       game_fns.update_bullets(ai_settings, screen, ship, aliens, bullets)
       game_fns.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-    game_fns.update_screen(ai_settings, screen, ship, aliens, bullets)
+    game_fns.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game() 
