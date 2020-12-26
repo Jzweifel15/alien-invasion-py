fc4e563083de2c30cfd57daf7ff5814ef301512b
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-26 18:36:10 -0500

    Game Over
    Added 3 lives to the ship. When the aliens hit the ship or the ground the ship loses a life and the aliens reset

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings 
 from ship import Ship
+from game_stats import GameStats
 
 import game_functions as game_fns
 
@@ -12,6 +13,9 @@ def run_game():
   ai_settings = Settings()
   screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
+
+  # Create an instance of store game statistics
+  stats = GameStats(ai_settings)
 
   # Make a ship, a group of bullets, and a group of aliens
   ship = Ship(ai_settings, screen)
@@ -24,9 +28,12 @@ def run_game():
   # Start the main loop for the game.
   while True:
     game_fns.check_events(ai_settings, screen, ship, bullets)
-    ship.update()
-    game_fns.update_bullets(ai_settings, screen, ship, aliens, bullets)
-    game_fns.update_aliens(ai_settings, aliens)
+
+    if stats.game_active:
+      ship.update()
+      game_fns.update_bullets(ai_settings, screen, ship, aliens, bullets)
+      game_fns.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
     game_fns.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
