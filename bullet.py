11f9f1578d0f7eebb9019ef94bad7b8c59e5b9d6
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-26 01:07:05 -0500

    Draw Bullet
    draws a bullet to the screen

--- bullet.py
+++ bullet.py
@@ -18,3 +18,16 @@ class Bullet(Sprite):
 
     self.color = ai_settings.bullet_color
     self.speed = ai_settings.bullet_speed
+
+
+  def update(self):
+    """ Move the bullet up the screen. """
+    # Update the decimal position of the bullet
+    self.y -= self.speed 
+    # Update the rect position
+    self.rect.y = self.y 
+
+
+  def draw_bullet(self):
+    """ Draw the bullet to the screen. """
+    pygame.draw.rect(self.screen, self.color, self.rect)
